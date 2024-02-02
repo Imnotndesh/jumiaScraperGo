6dc90ec09d427c756f89e45867279eb308f1ef28
imnotndesh <slateinbox@gmail.com>
2024-02-02 22:28:43 +0300

    added item entry

--- jumiaScraper.go
+++ jumiaScraper.go
@@ -3,16 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
-
 	"github.com/gocolly/colly/v2"
 )
 type productInfo struct{
 	Name ,Price, url string
 }
-func main(){
-	println("Starting task")
+func fetcher(itemName string){
 	jumiaColly := colly.NewCollector()
-	url := "http://jumia.co.ke/catalog/?q=shoes&sort=lowest-price&shipped_from=country_local"
+	url := "http://jumia.co.ke/catalog/?q="+itemName+"&sort=lowest-price&shipped_from=country_local"
 	
 	jumiaColly.OnHTML("a.core", func(h *colly.HTMLElement) {
 		// Instance of the struct above
@@ -22,17 +20,24 @@ func main(){
 		prodInfo.Name = h.ChildText("h3")
 		prodInfo.Price = h.ChildText(".prc")
 		prodInfo.url = "Http://jumia"+h.Attr("href")
-
+		fmt.Println(" ")
+		fmt.Println("....")
 		fmt.Printf("Name: %s, \nPrice: %s , \nUrl: %s\n",prodInfo.Name,prodInfo.Price,prodInfo.url )
 		fmt.Println("....")
+		fmt.Println(" ")
 	})
 	jumiaColly.OnError(func(r *colly.Response, err error) {
 		log.Println("Error: ",err)
 	})
-
-	err := jumiaColly.Visit(url)
-	if err != nil {
-		log.Fatal("Error visiting url: ",err)
-	}
-	println("Ended task")
+	jumiaColly.Visit(url)
+}
+func main(){
+	println("Starting fetcher...")
+	var passedUserInput string
+	println("Enter an item name or category... ")
+	fmt.Scanln(&passedUserInput)
+	fmt.Println("Finding..."+passedUserInput)
+	fetcher(passedUserInput)
+	println(" ")
+	println("Closing fetcher")
 }
