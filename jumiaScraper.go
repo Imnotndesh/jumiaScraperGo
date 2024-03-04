1ffe73e376dda6b0c70d10621b365f72d42fef49
Imnotndesh <slateinbox@gmail.com>
2024-03-04 14:16:36 +0300

    added better logic

--- jumiaScraper.go
+++ jumiaScraper.go
@@ -11,25 +11,32 @@ type productInfo struct {
 	Name, Price, url string
 }
 
+var itemData []productInfo
+//var allData []productInfo
+
 func getItemFromJumia(url string) {
 	jumiaColly := colly.NewCollector()
+
 	jumiaColly.OnHTML("a.core", func(h *colly.HTMLElement) {
-		// Instance of the struct above
-		prodInfo := productInfo{}
 
 		// Taking product info off page
-		prodInfo.Name = h.ChildText("h3")
-		prodInfo.Price = h.ChildText(".prc")
-		prodInfo.url = "Http://jumia" + h.Attr("href")
-		fmt.Println(" ")
-		fmt.Println("....")
-		fmt.Printf("Name: %s, \nPrice: %s , \nUrl: %s\n", prodInfo.Name, prodInfo.Price, prodInfo.url)
-		fmt.Println("....")
+		itemName := h.ChildText("h3")
+		itemPrice := h.ChildText(".prc")
+		itemUrl := "Http://jumia" + h.Attr("href")
+
+		collData := productInfo{
+			Name: itemName,
+			Price: itemPrice,
+			url: itemUrl, 
+		}
+		itemData = append(itemData, collData)
+
 	})
 	jumiaColly.OnError(func(r *colly.Response, err error) {
 		log.Println("Error: ", err)
 	})
 	jumiaColly.Visit(url)
+	fmt.Println(itemData[0])
 }
 func generateUrl(itemName string) {
 	url := "http://jumia.co.ke/catalog/?q=" + itemName + "&sort=lowest-price&shipped_from=country_local"
@@ -39,7 +46,7 @@ func generateUrl(itemName string) {
 func startScrape() {
 	var passedUserInput string
 	fmt.Println("Fetching.......")
-	flag.StringVar(&passedUserInput, "item", "","Item to fetch")
+	flag.StringVar(&passedUserInput, "item", "", "Item to fetch")
 	flag.Parse()
 	generateUrl(passedUserInput)
 }
